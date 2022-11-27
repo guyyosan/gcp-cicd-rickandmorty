836d147abc5357e25860648f44fbf968b80575e6
guyyosan <guyma@google.com>
2022-11-27 11:52:46 +0200

    123

--- tests.py
+++ tests.py
@@ -6,5 +6,8 @@ class TestStringMethods(unittest.TestCase):
     def test_get_chars(self):
         self.assertEqual('FOO', 'FOO')
 
+    def test_get_failure(self):
+        self.assertEqual('FOO', 'FOO1')
+
 if __name__ == '__main__':
     unittest.main()
